e7a4fb125f8f2b4b1675bf0809592ed774949db4
Jasmit Tarang <jasmit.tarang@goteleport.com>
2025-03-21 14:57:23 -0500

    Add support for mongodb, use mongosh to resolve and ping the db

--- getavail.sh
+++ getavail.sh
@@ -2,29 +2,48 @@
 #usage: getavail.sh <HOST> <PORT> --outputnmap
 
 if [ "$1" == "" ] || [ "$2" == "" ]   ; then
-	echo "Calls nmap to check for available service with given service type"
-	echo "Usage: $(basename $0) getavail.sh <HOST> <PORT> <service type to confirm : mysql> <output map output --outputnmap>"
-	exit 1
+       echo "Calls nmap to check for available service with given service type"
+       echo "Usage: $(basename $0) getavail.sh <HOST> <PORT> <service type to confirm : mysql> <output map output --outputnmap>"
+       exit 1
 fi
 
 # Confirms nmap is available 
-if ! command -v nmap &> /dev/null
-then
-    echo "❓no nmap to confirm"
-    exit
+if ! command -v nmap &> /dev/null; then
+    echo "⚠️  no nmap to confirm"
+    exit 1
 fi
-
 
 HOST_TO_CHECK=$1
 PORT_TO_CHECK=$2
 FORMAT_TO_CHECK=$3
 
-#Health output
+# Health output
 HEALTHY_CONNECTION="Ok 🟢"
 NO_CONNECTION="Unavailable ❌"
-NMAP_RESULTS=$(nmap -p $PORT_TO_CHECK -Pn -sV $HOST_TO_CHECK )
-if [ "$3" == "--outputnmap" ] ; then
-	echo $NMAP_RESULTS;
-fi ;
 
-if echo "$NMAP_RESULTS"  | grep -i -m 1 "$PORT_TO_CHECK.*open" &> /dev/null ;then  echo "$HEALTHY_CONNECTION"; else echo "$NO_CONNECTION";  fi ;
+# Run nmap scan
+NMAP_RESULTS=$(nmap -p $PORT_TO_CHECK -Pn -sV $HOST_TO_CHECK)
+
+# Check if the service type is MongoDB
+if [[ "$HOST_TO_CHECK" == mongodb* ]]; then
+    # Check if mongosh is installed
+    if ! command -v mongosh &> /dev/null; then
+        echo "⚠️  no mongosh to confirm"
+        exit 1
+    fi
+
+    MONGO_OUTPUT=$(mongosh "$HOST_TO_CHECK" --eval "db.runCommand({ ping: 1 })" --quiet)
+
+    if echo "$MONGO_OUTPUT" | grep -q '{ ok: 1 }' &> /dev/null; then
+        echo "$HEALTHY_CONNECTION"
+    else
+        echo "$NO_CONNECTION"
+    fi
+else
+    # If not MongoDB, just check the Nmap result
+    if echo "$NMAP_RESULTS" | grep -i -m 1 "$PORT_TO_CHECK.*open" &> /dev/null; then
+        echo "$HEALTHY_CONNECTION"
+    else
+        echo "$NO_CONNECTION"
+    fi
+fi
